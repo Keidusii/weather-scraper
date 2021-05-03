327ea5bdc403c5d81672a3e730bcb6d088c8c3df
Keidusii <mhallii@umich.edu>
2021-05-02 22:15:10 -0400

    renaming files

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <link rel="stylesheet" href="6-php/scraper.css">
+        <link rel="stylesheet" href="files/scraper.css">
     </head>
     <body>
         <h1>What's The Weather?</h1>
@@ -113,6 +113,6 @@
         <!--Scripts-->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-        <script src="6-php/ws.js"></script>
+        <script src="files/ws.js"></script>
     </body>
 </html>
